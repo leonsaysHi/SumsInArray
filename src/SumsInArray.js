147c841b012a119e716d37a32c36f834f7ed6bf7
leonsaysHi <uoa7.work@gmail.com>
2017-05-19 01:52:06 +0200

    forgot same numeric element pairs are possible

--- src/SumsInArray.js
+++ src/SumsInArray.js
@@ -2,7 +2,8 @@
 
 const SumsInArray = (arr, arg) => arr
     .reduce((accu, num, idx) => {
-        if (num < arg && !accu.find(o => num === o.num)) {
+        const sameNum = accu.filter(item => item.num === num)
+        if (num < arg && sameNum.length < 2) {
             accu.push({
                 idx,
                 num
@@ -13,8 +14,8 @@ const SumsInArray = (arr, arg) => arr
     .sort((a, b) => a.num - b.num)
     .reduce((accu, item1, idx, _arr) => {
         const item2 = _arr
-            .slice(idx+1)
-            .find (_item => _item.num + item1.num === arg)
+            .slice(idx + 1)
+            .find(_item => _item.num + item1.num === arg)
         if (item2) {
             console.log(item1, item2)
             accu += item1.idx + item2.idx
