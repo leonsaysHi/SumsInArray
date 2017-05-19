a2e2192d51f37ef82ff66f63f4b9f29fce2857f4
leonsaysHi <uoa7.work@gmail.com>
2017-05-19 02:59:42 +0200

    now meets all requierments

--- src/SumsInArray.js
+++ src/SumsInArray.js
@@ -2,25 +2,32 @@
 
 const SumsInArray = (arr, arg) => arr
     .reduce((accu, num, idx) => {
-        const sameNum = accu.filter(item => item.num === num)
-        if (num < arg && sameNum.length < 2) {
-            accu.push({
-                idx,
-                num,
-                used: false
-            })
+        if (num < arg) {
+            const item = accu.find(item => item.num === num)
+            if (!item) {
+                accu.push({
+                    idxs: [idx],
+                    num
+                })
+            }
+            else if (item.idxs.length < 2) {
+                item.idxs.push(idx)
+            }
         }
         return accu
     }, [])
     .sort((a, b) => a.num - b.num)
     .reduce((accu, item1, idx, _arr) => {
-        const item2 = _arr
-            .slice(idx + 1)
-            .find(_item => !_item.used && _item.num + item1.num === arg)
-        if (item2) {
-            console.log(item1, item2)
-            item2.used = true
-            accu += item1.idx + item2.idx
+        if (item1.num < arg * .5) {
+            const item2 = _arr.find(item => item.num + item1.num === arg && item.idxs.length > 0)
+            if (item2) {
+                while (item1.idxs.length && item2.idxs.length) {
+                    accu += item1.idxs.shift() + item2.idxs.shift()
+                }
+            }
+        }
+        else if (item1.num === arg * .5 && item1.idxs.length === 2) {
+            accu += item1.idxs.reduce((a, b) => a + b)
         }
         return accu
     }, 0)
